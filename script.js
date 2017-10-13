b098abdd85c3f0ba54dce7fe14cb42e76de16029
codingbirk <32701575+codingbirk@users.noreply.github.com>
2017-10-13 11:26:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+document.getElementById('test').innerHTML = "I put text here";
+
+if (5>6){
+	console.log("it is true");
+}
+
+else {
+	console.log("it is false");
+}
+
+function printApples(unknown){
+	while (unknown<15){
+		console.log("I Have an apple")
+		unknown+=1;
+	}
+
+}
+
+printApples(10);
